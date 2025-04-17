bd815567e3c4a919deef7796db3d3d4acb2e734d
aapolipponen <aapo.lipponen@gmail.com>
2025-04-17 19:27:03 +0300

    Refactor game logic and improve code structure

--- animframe_polyfill.js
+++ animframe_polyfill.js
@@ -1,27 +1,28 @@
-(function() {
-    var lastTime = 0;
-    var vendors = ['webkit', 'moz'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-      window.cancelAnimationFrame =
-      window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
+(() => {
+  let lastTime = 0;
+  const vendors = ['webkit', 'moz'];
   
-    if (!window.requestAnimationFrame) {
-      window.requestAnimationFrame = function(callback, element) {
-        var currTime = new Date().getTime();
-        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-        var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-        timeToCall);
-        lastTime = currTime + timeToCall;
-        return id;
-      };
-    }
-  
-    if (!window.cancelAnimationFrame) {
-      window.cancelAnimationFrame = function(id) {
-        clearTimeout(id);
-      };
-    }
-  }());
-  
+  for (const vendor of vendors) {
+    if (window.requestAnimationFrame) break;
+    window.requestAnimationFrame = window[`${vendor}RequestAnimationFrame`];
+    window.cancelAnimationFrame = 
+      window[`${vendor}CancelAnimationFrame`] || window[`${vendor}CancelRequestAnimationFrame`];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = callback => {
+      const currTime = Date.now();
+      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      const id = setTimeout(() => {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = id => clearTimeout(id);
+  }
+})();
--- tile.js
+++ tile.js
@@ -1,18 +1,17 @@
-function Tile(position, value) {
-    this.x                = position.x;
-    this.y                = position.y;
-    this.value            = value || 2;
-  
-    this.previousPosition = null;
-    this.mergedFrom       = null; // Tracks tiles that merged together
-  }
-  
-  Tile.prototype.savePosition = function () {
-    this.previousPosition = { x: this.x, y: this.y };
-  };
-  
-  Tile.prototype.updatePosition = function (position) {
+class Tile {
+  constructor(position, value = 2) {
     this.x = position.x;
     this.y = position.y;
-  };
-  
+    this.value = value;
+    this.previousPosition = null;
+    this.mergedFrom = null;
+  }
+
+  savePosition() {
+    this.previousPosition = { x: this.x, y: this.y };
+  }
+
+  updatePosition(position) {
+    [this.x, this.y] = [position.x, position.y];
+  }
+}
